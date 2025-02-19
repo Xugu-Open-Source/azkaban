9bd979699547e31fe366ff6592ecafc1bfdcda10
WANG Qing <2362072650@qq.com>
2025-02-19 13:35:58 +0800

    添加对 Xugu 数据库的支持

--- azkaban-common/src/main/java/azkaban/AzkabanCommonModule.java
+++ azkaban-common/src/main/java/azkaban/AzkabanCommonModule.java
@@ -19,10 +19,12 @@ package azkaban;
 import azkaban.db.AzkabanDataSource;
 import azkaban.db.H2FileDataSource;
 import azkaban.db.MySQLDataSource;
+import azkaban.db.XuguDataSource;
 import azkaban.executor.ExecutorLoader;
 import azkaban.executor.JdbcExecutorLoader;
 import azkaban.project.JdbcProjectImpl;
 import azkaban.project.ProjectLoader;
+import azkaban.project.XuguJdbcProjectImpl;
 import azkaban.spi.Storage;
 import azkaban.spi.StorageException;
 import azkaban.storage.StorageImplementationType;
@@ -56,10 +58,15 @@ public class AzkabanCommonModule extends AbstractModule {
   @Override
   protected void configure() {
     install(new AzkabanCoreModule(this.props));
+    String dataBaseType = props.getString("database.type");
+    if ( "xugu".equalsIgnoreCase(dataBaseType)){
+      bind(ProjectLoader.class).to(XuguJdbcProjectImpl.class);
+    }else {
+      bind(ProjectLoader.class).to(JdbcProjectImpl.class);
+    }
     bind(Storage.class).to(resolveStorageClassType());
     bind(AzkabanDataSource.class).to(resolveDataSourceType());
     bind(TriggerLoader.class).to(JdbcTriggerImpl.class);
-    bind(ProjectLoader.class).to(JdbcProjectImpl.class);
     bind(ExecutorLoader.class).to(JdbcExecutorLoader.class);
   }
 
@@ -89,6 +96,8 @@ public class AzkabanCommonModule extends AbstractModule {
     final String databaseType = this.props.getString("database.type");
     if (databaseType.equals("h2")) {
       return H2FileDataSource.class;
+    } else if (databaseType.equals("xugu")) {
+      return XuguDataSource.class;
     } else {
       return MySQLDataSource.class;
     }
