e4369fffd4992792ad4cd45201840153d842b3aa
WANG Qing <2362072650@qq.com>
2025-02-19 13:34:57 +0800

    添加虚谷sql脚本

---
+++ azkaban-db/src/main/xugu/create.executors.sql
@@ -0,0 +1,8 @@
+CREATE TABLE executors (
+  id     INT         IDENTITY NOT NULL PRIMARY KEY,
+  host   VARCHAR(64) NOT NULL,
+  port   INT         NOT NULL,
+  active BOOLEAN                          DEFAULT FALSE,
+  UNIQUE (host, port),
+  UNIQUE (id)
+);
---
+++ azkaban-db/src/main/xugu/create.projects.sql
@@ -0,0 +1,16 @@
+CREATE TABLE projects (
+  id               INT         IDENTITY NOT NULL PRIMARY KEY,
+  name             VARCHAR(64) NOT NULL,
+  active           BOOLEAN,
+  modified_time    BIGINT      NOT NULL,
+  create_time      BIGINT      NOT NULL,
+  version          INT,
+  last_modified_by VARCHAR(64) NOT NULL,
+  description      VARCHAR(2048),
+  enc_type         TINYINT,
+  settings_blob    BLOB,
+  UNIQUE (id)
+);
+
+CREATE INDEX project_name
+  ON projects (name);
